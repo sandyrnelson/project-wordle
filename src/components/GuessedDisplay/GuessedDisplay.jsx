a206e711396f9493c3b21f4b57d505f773db5a53
Sandy Nelson <sandy.nelson@nexius.hu>
2024-12-19 16:08:41 +0100

    Remove logs and unused import

--- src/components/GuessedDisplay/GuessedDisplay.jsx
+++ src/components/GuessedDisplay/GuessedDisplay.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { NUM_OF_GUESSES_ALLOWED, WORD_LENGTH_ALLOWED } from '../../constants';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 import { range } from '../../utils';
 import GuessedSlot from '../GuessedSlot/GuessedSlot';
 
 function GuessedDisplay({ guesses, answer }) {
-	console.log('guesses:', guesses);
-
 	return (
 		<div className='guess-results'>
 			{range(NUM_OF_GUESSES_ALLOWED).map((wordIndex) => (
