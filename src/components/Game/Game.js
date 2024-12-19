b901e18f4b5095aad6ece7248824889dcaa11bec
Sandy Nelson <sandy.nelson@nexius.hu>
2024-12-19 16:10:29 +0100

    Implement game win, lose logic with banner display

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,9 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
 import GuessedDisplay from '../GuessedDisplay/GuessedDisplay';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import GameWon from '../GameWon/GameWon';
+import GameLost from '../GameLost/GameLost';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,13 +15,26 @@ console.info({ answer });
 
 function Game() {
 	const [guesses, setGuesses] = useState([]);
-  function updateGuessList(newGuess) {
-    setGuesses([...guesses, newGuess])
-  } 
+	const [gameStatus, setGameStatus] = useState('running');
+
+	function updateGuessList(newGuess) {
+		const nextGuesses = [...guesses, newGuess];
+		setGuesses(nextGuesses);
+
+		if (newGuess === answer) {
+			setGameStatus('won');
+		} else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+			setGameStatus('lost');
+		}
+	}
 	return (
 		<>
 			<GuessedDisplay guesses={guesses} answer={answer} />
-			<GuessInput updateGuessList={updateGuessList} />
+			{gameStatus === 'running' && (
+				<GuessInput updateGuessList={updateGuessList} />
+			)}
+			{gameStatus === 'won' && <GameWon guessesNum={guesses.length} />}
+			{gameStatus === 'lost' && <GameLost answer={answer} />}
 		</>
 	);
 }
